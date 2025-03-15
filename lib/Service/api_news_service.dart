dafd613feba4ebe1997d5e7cfd52978e41d27e41
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2025-03-15 14:44:53 +0600

    first commit: api collected

---
+++ lib/Service/api_news_service.dart
@@ -0,0 +1,7 @@
+class ApiService{
+
+  final all_news_url="https://newsapi.org/v2/everything?q=bitcoin&apiKey=1c1939cea26d41e09eae408144fcffd1";
+  final breaking_news_url="https://newsapi.org/v2/top-headlines?country=us&apiKey=1c1939cea26d41e09eae408144fcffd1";
+
+
+}
